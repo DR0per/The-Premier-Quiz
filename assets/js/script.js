1f27b5782b6c528684b7ad208944fc9cfdf44a6d
DR0per <>
2023-06-25 13:32:33 +0000

    Add proper formatting to the HTML,CSS & JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 /**  JavaScript code for quiz functionality **/
-const submitButton = document.getElementById('submitBtn');
-const quizSection = document.getElementById('quiz');
-const resultsSection = document.getElementById('results');
-const scoreElement = document.getElementById('score');
+const submitButton = document.getElementById("submitBtn");
+const quizSection = document.getElementById("quiz");
+const resultsSection = document.getElementById("results");
+const scoreElement = document.getElementById("score");
 
-submitButton.addEventListener('click', showResults);
+submitButton.addEventListener("click", showResults);
 
 function showResults() {
-    const questions = document.getElementsByClassName('question');
+    const questions = document.getElementsByClassName("question");
     let score = 0;
 
     for (let i = 0; i < questions.length; i++) {
@@ -25,26 +25,25 @@ function showResults() {
     }
 
     /**Display the score**/
-    scoreElement.textContent = 'Your score: ' + score + '/' + questions.length;
+    scoreElement.textContent = "Your score: " + score + "/" + questions.length;
 
     /**Hide the quiz section and show the results section **/
-    quizSection.style.display = 'none';
-    resultsSection.style.display = 'block';
+    quizSection.style.display = "none";
+    resultsSection.style.display = "block";
 }
 
 function getCorrectAnswer(questionIndex) {
     const correctAnswers = [
-        'France',             /** Correct answer for question 1 **/
-        'Lionel Messi',       /** Correct answer for question 2 **/
-        'Brazil',             /** Correct answer for question 3 **/
-        'Cristiano Ronaldo',  /** Correct answer for question 4 **/ 
-        'Italy',              /** Correct answer for question 5 **/ 
-        'Manchester United',  /** Correct answer for question 6 **/ 
-        'Alan Shearer',       /** Correct answer for question 7 **/ 
-        'Manchester City',    /** Correct answer for question 8 **/ 
-        'Wayne Rooney',       /** Correct answer for question 9 **/ 
-        'Blackburn Rovers'    /** Correct answer for question 10 **/ 
-        
+        "France" /** Correct answer for question 1 **/,
+        "Lionel Messi" /** Correct answer for question 2 **/,
+        "Brazil" /** Correct answer for question 3 **/,
+        "Cristiano Ronaldo" /** Correct answer for question 4 **/,
+        "Italy" /** Correct answer for question 5 **/,
+        "Manchester United" /** Correct answer for question 6 **/,
+        "Alan Shearer" /** Correct answer for question 7 **/,
+        "Manchester City" /** Correct answer for question 8 **/,
+        "Wayne Rooney" /** Correct answer for question 9 **/,
+        "Blackburn Rovers" /** Correct answer for question 10 **/,
     ];
 
     return correctAnswers[questionIndex];
@@ -52,25 +51,24 @@ function getCorrectAnswer(questionIndex) {
 
 document.getElementById("tryAgainBtn").addEventListener("click", resetQuiz);
 
-        function resetQuiz() {
-            /** Clear selected answers **/
-            var selectedAnswers = document.querySelectorAll('input[type="radio"]:checked');
-            for (var i = 0; i < selectedAnswers.length; i++) {
-                selectedAnswers[i].checked = false;
-            }
+function resetQuiz() {
+    /** Clear selected answers **/
+    var selectedAnswers = document.querySelectorAll('input[type="radio"]:checked');
+    for (var i = 0; i < selectedAnswers.length; i++) {
+        selectedAnswers[i].checked = false;
+    }
 
-            /** Hide the results section **/
-            resultsSection.style.display = "none";
-            /**  Show the quiz section **/
-            quizSection.style.display = "block";
-        }
-
+    /** Hide the results section **/
+    resultsSection.style.display = "none";
+    /**  Show the quiz section **/
+    quizSection.style.display = "block";
+}
 
 /** Set the duration of the quiz in seconds */
 const quizDuration = 120; // 2 minutes
 
 /** Get the timer element from the HTML */
-const timerElement = document.getElementById('timer');
+const timerElement = document.getElementById("timer");
 
 let deadline = null; /** Store the quiz deadline */
 
@@ -87,7 +85,7 @@ function updateTimer() {
     const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
     /** Display the remaining time */
-    timerElement.textContent = `Time remaining: ${minutes}:${seconds.toString().padStart(2, '0')}`;
+    timerElement.textContent = `Time remaining: ${minutes}:${seconds.toString().padStart(2, "0")}`;
 
     /** Check if the time has run out */
     if (remainingTime <= 0) {
@@ -117,27 +115,27 @@ function resetQuiz() {
     }
 
     /** Hide the results section **/
-    const resultsSection = document.getElementById('results');
-    resultsSection.style.display = 'none';
+    const resultsSection = document.getElementById("results");
+    resultsSection.style.display = "none";
 
     /** Show the quiz section **/
-    const quizSection = document.getElementById('quiz');
-    quizSection.style.display = 'block';
+    const quizSection = document.getElementById("quiz");
+    quizSection.style.display = "block";
 
     /** Restart the timer */
     startTimer();
 }
 
 /** Call the startTimer function when the page is loaded */
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener("DOMContentLoaded", function () {
     startTimer();
 });
 
 /** Get the "Try Again" button element */
-const tryAgainButton = document.getElementById('tryAgainBtn');
+const tryAgainButton = document.getElementById("tryAgainBtn");
 
 /** Add click event listener to the "Try Again" button */
-tryAgainButton.addEventListener('click', function () {
+tryAgainButton.addEventListener("click", function () {
     /** Call the resetQuiz function when the "Try Again" button is clicked */
     resetQuiz();
 });
