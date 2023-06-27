57d6e3c9b6343fc4efecd46cf5af516c2809152b
DR0per <>
2023-06-27 13:15:05 +0000

    Add new code to JavaScript its still not working when I try to get the Correct Answers to match up with the random questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,32 +6,43 @@ const scoreElement = document.getElementById("score");
 
 submitButton.addEventListener("click", showResults);
 
-/** Function to Randomize Questions for the user */
 function randomizeQuestions() {
-    let questionContainer = document.getElementById('questionContainer');
-    let questions = Array.from(questionContainer.getElementsByClassName('question'));
-    
-    // Randomly select and display 15 questions
-    let selectedQuestions = shuffleArray(questions).slice(0, 15);
-    
-    // Clear the container
-    questionContainer.innerHTML = '';
-    
-    // Append the selected questions to the container
-    for (let i = 0; i < selectedQuestions.length; i++) {
-      questionContainer.appendChild(selectedQuestions[i]);
+    // Get all the question elements
+    var questions = document.getElementsByClassName('question');
+  
+    // Convert the HTMLCollection to an array for easier manipulation
+    var questionsArray = Array.from(questions);
+  
+    // Shuffle the array using Fisher-Yates algorithm
+    for (var i = questionsArray.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = questionsArray[i];
+      questionsArray[i] = questionsArray[j];
+      questionsArray[j] = temp;
     }
+  
+    // Append the shuffled questions back to the parent element
+    var parentElement = questions[0].parentNode;
+    parentElement.innerHTML = '';
+    questionsArray.forEach(function(question) {
+      parentElement.appendChild(question);
+    });
+  
+    // Add event listeners to the radio buttons
+    var radioButtons = document.querySelectorAll('input[type="radio"]');
+    radioButtons.forEach(function(radioButton) {
+      radioButton.addEventListener('change', function() {
+        // Check if the selected answer is correct
+        if (this.checked && this.getAttribute('data-correct-answer') === this.value) {
+          console.log('Correct!');
+        } else {
+          console.log('Incorrect!');
+        }
+      });
+    });
   }
   
-  /** Shuffle an array using Fisher-Yates algorithm */
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  }
-  
+  // Call the function to randomize the questions
   randomizeQuestions();
 
 function showResults() {
@@ -44,7 +55,7 @@ function showResults() {
 
         if (selectedOption !== null) {
             const selectedValue = selectedOption.value;
-            const correctAnswer = getCorrectAnswer(i);
+            const correctAnswer = getCorrectAnswer(i + 1); // Add 1 to the index to match the question number
 
             if (selectedValue === correctAnswer) {
                 score += 1;
@@ -61,31 +72,113 @@ function showResults() {
 }
 
 function getCorrectAnswer(questionIndex) {
-    const correctAnswers = [
-        "France", /** Correct answer for question 1 **/
-        "Lionel Messi", /** Correct answer for question 2 **/
-        "Brazil", /** Correct answer for question 3 **/
-        "Cristiano Ronaldo", /** Correct answer for question 4 **/
-        "Italy", /** Correct answer for question 5 **/
-        "Manchester United", /** Correct answer for question 6 **/
-        "Alan Shearer", /** Correct answer for question 7 **/
-        "Manchester City", /** Correct answer for question 8 **/
-        "Wayne Rooney", /** Correct answer for question 9 **/
-        "Manchester United", /** Correct answer for question 10 **/
-        "Erik ten Hag", /** Correct answer for question 11 **/
-        "Pep Guardiola", /** Correct answer for question 12 **/
-        "PSG", /** Correct answer for question 13 **/
-        "Juventus", /** Correct answer for question 14 **/
-        "2003", /** Correct answer for question 15 **/
-        "1999", /** Correct answer for question 16 **/
-        "03-04", /** Correct answer for question 17 **/
-        "Manchester City", /** Correct answer for question 18 **/
-        "Ryan Giggs", /** Correct answer for question 19 **/
-        "Richard Dunne" /** Correct answer for question 20 **/
+    let quizQuestions = [
+      {
+        text: "Question: Who won the FIFA World Cup 2018?",
+        options: ["France", "Brazil", "Germany"],
+        correctAnswer: "France"
+      },
+      {
+        text: "Question: Which player has won the most Ballon d'Or awards?",
+        options: ["Lionel Messi", "Cristiano Ronaldo", "Pele"],
+        correctAnswer: "Lionel Messi"
+      },
+      {
+        text: "Question: Which country has won the most FIFA World Cup titles?",
+        options: ["Brazil", "Germany", "Italy"],
+        correctAnswer: "Brazil"
+      },
+      {
+        text: "Who is the all-time leading goal scorer in international matches?",
+        options: ["Christiano Ronaldo", "Pele", "Marta"],
+        correctAnswer: "Christiano Ronaldo"
+      },
+      {
+        text: "Which country won the UEFA Euro 2020 tournament?",
+        options: ["Italy", "England", "France"],
+        correctAnswer: "Italy"
+      },
+      {
+        text: "Question: Which team has won the most Premier League titles?",
+        options: ["Manchester United", "Liverpool", "Chelsea", "Arsenal"],
+        correctAnswer: "Manchester United"
+      },
+      {
+        text: "Question: Who is the all-time leading goalscorer in the Premier League?",
+        options: ["Alan Shearer", "Thierry Henry", "Wayne Rooney", "Sergio Aguero"],
+        correctAnswer: "Alan Shearer"
+      },
+      {
+        text: "Question: Which team holds the record for the most points in a single Premier League season?",
+        options: ["Manchester United", "Manchester City", "Chelsea", "Liverpool"],
+        correctAnswer: "Manchester City"
+      },
+      {
+        text: "Question: Who is the youngest player to score a Premier League hat-trick?",
+        options: ["Michael Owen", "Wayne Rooney", "Robbie Fowler", "Francis Jeffers"],
+        correctAnswer: "Wayne Rooney"
+      },
+      {
+        text: "Question: Which club won the first-ever Premier League season in 1992-1993?",
+        options: ["Manchester United", "Arsenal", "Blackburn Rovers", "Liverpool"],
+        correctAnswer: "Manchester United"
+      },
+      {
+        text: "Question: Who is the current manager of Manchester United?<",
+        options: ["Jurgen Klopp", "Erik ten Hag", "Roy Keane", "Sir Alex Ferguson"],
+        correctAnswer: "Erik ten Hag"
+      },
+      {
+        text: "Question: Who is the current manager of Manchester City?",
+        options: ["Erik ten Hag", "Mikel Arteta", "Jurgen Klopp", "Pep Guardiola"],
+        correctAnswer: "Pep Guardiola"
+      },
+      {
+        text: "Question: Which team did Lionel Messi join after leaving Barcelona?",
+        options: ["Manchester United", "Real Madrid", "PSG", "Liverpool"],
+        correctAnswer: "PSG"
+      },
+      {
+        text: "Question: Which club did Christiano Ronaldo join after leaving Real Madrid?",
+        options: ["Manchester United", "Arsenal", "Juventus", "Bayern Munich"],
+        correctAnswer: "Juventus"
+      },
+      {
+        text: "Question: Roman Abramovich purchased Chelsea Football Club in which year?",
+        options: ["2000", "2001", "2002", "2003"],
+        correctAnswer: "2003"
+      },
+      {
+        text: "Question: Manchester United won the Treble in which year?",
+        options: ["1998", "1999", "2000", "2001"],
+        correctAnswer: "1999"
+      },
+      {
+        text: "Question: In what season did Arsenal become known as 'The Invincibles'?",
+        options: ["03-04", "98-99", "02-03", "04-05"],
+        correctAnswer: "03-04"
+      },
+      {
+        text: "Question: Which English club won the treble in the 22/23 season?",
+        options: ["Manchester United", "Arsenal", "PManchester City", "Liverpool"],
+        correctAnswer: "Manchester City"
+      },
+      {
+        text: "Question: Which player has the most assists in the Premier League?",
+        options: ["Wayne Rooney", "Kevin De Bruyne", "Cesc Fabregas", "Ryan Giggs"],
+        correctAnswer: "Ryan Giggs"
+      },
+      {
+        text: "Question: Which player has the most own goals scored in the Premier League?",
+        options: ["Gary Neville", "Jamie Carragher", "Richard Dunne", "John Terry"],
+        correctAnswer: "Richard Dunne"
+      },
     ];
+  
+    const question = quizQuestions[questionIndex];
+    return question ? question.correctAnswer : null;
+  }
 
-    return correctAnswers[questionIndex];
-}
 
 document.getElementById("tryAgainBtn").addEventListener("click", resetQuiz);
 
